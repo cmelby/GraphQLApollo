45d4c0e09fc58ba5b948af0ef4954f5bfc4dd33e
Christopher Melby <melby.christopher@gmail.com>
2020-04-09 08:10:55 -0700

    Pokemons container completed. gql query completed returning first 9 characters

--- containers/PokemonsContainer.js
+++ containers/PokemonsContainer.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Pokemon } from '../components/Pokemon'
 
 export function PokemonsContainer() {
+    const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+        varibles: { first: 9 }
+    }); 
     return (
-        <p>I am a pokemon container</p>  
+        <div className = "container">
+            {pokemons && pokemons.map(pokemon => 
+                    <Pokemon 
+                        key={pokemon.id} 
+                        pokemon={pokemon} 
+                    />)}
+        </div> 
     );
 }
+
+// 1) go ahead and add a key to the pokemon component
+// 2) go ahead and pass a prop of 'pokemon'
